fab9f60228e8c5196b2cb21d538fb612c4e08f6d
He Pufan <pufan@google.com>
2014-08-21 18:40:56 +0800

    Today's work

--- game.h
+++ game.h
@@ -1,3 +1,6 @@
+#ifndef GAME_H_
+#define GAME_H_
+
 #include <string>
 #include <vector>
 #include <map>
@@ -5,53 +8,130 @@
 
 #include "register.h"
 
-namespace square_merge_game {
+class GameBoard;
+class GameState;
 
-class ScoreMethod {
+class Move: RegisterBase<Move> {
+ public:
+  virtual const char* info() override {
+    return "Type of possible interractions for the game.";
+  }
+
+  // Returns true if input captured. Cleans up the input buffer.
+  virtual bool check() {
+    return false;
+  }
 };
 
-class MoveMethod {
+class MoveMethod: RegisterBase<MoveMethod> {
+ public:
+  virtual const char* info() override {
+    return "The method to perform different moves.";
+  }
 };
 
-class TextMethod {
+class TextMethod: RegisterBase<TextMethod> {
+ public:
+  virtual const char* info() override {
+    return "Versions of all text contents in the game.";
+  }
+
+  // Use own texts by a set of custom entries in the game.
+  virtual const char* get_text(std::string entry) {
+    return "";
+  }
 };
 
-class Goal {
+class Event: RegisterBase<Event> {
+ public:
+  virtual const char* info() override {
+    return "Different events in the game.";
+  }
+
+  // Returns true if event captured.
+  virtual bool check(const GameState&) = 0;
+};
+
+class WinEvent: Event {
+ public:
+  virtual const char* info() override {
+    return "Game winning conditions.";
+  }
+
+  // Returns true if win.
+  virtual bool check(const GameState&) override {
+    return false;
+  }
+};
+
+class LoseEvent: Event {
+ public:
+  virtual const char* info() override {
+    return "Game losing conditions.";
+  }
+
+  // Returns true if lose.
+  virtual bool check(const GameState&) override {
+    return false;
+  }
+};
+
+class ScoreEvent: Event {
+ public:
+  virtual const char* info() override {
+    return "Methods to score the game.";
+  }
+
+  // Returns true if score need to update.
+  virtual bool check(const GameState&) override {
+    return false;
+  }
 };
 
 struct GameOptions {
   int rand_seed;
   int game_size;
   int max_undo;
-  std::unique_ptr<TextMethod> text_method;
-  std::unique_ptr<ScoreMethod> score_method;
-  std::unique_ptr<MoveMethod> move_method;
-  std::unique_ptr<Goal> goal;
+  TextMethod::ptr text_method;
+  MoveMethod::ptr move_method;
+  WinEvent::ptr win_event;
+  LoseEvent::ptr lose_event;
+  std::vector<Move::ptr> move;
 };
 
 class GameBoard {
+ public:
+  std::vector<int> board;
 };
 
 class GameState {
+ public:
+  GameOptions options;
   std::vector<GameBoard> history;
+
+  const char* save_state();
+
+  bool load_state(std::string state_string);
 };
 
 class SquareMergeGame {
  public:
-  SquareMergeGame(const GameOptions& options);
-  const GameOptions& options() {
-    return options_;
+  SquareMergeGame(const GameOptions& options) {
+    state.options = options;
   }
-  virtual void SetRules();
+  const GameOptions& options() {
+    return state.options;
+  }
+
+  bool advance();
+
+  const char* help();
 
  protected:
   SquareMergeGame() {}
 
  private:
-  GameOptions options_;
+  GameState state;
 };
 
-class Game2048: public SquareMergeGame {
-};
-
-};  // namespace square_merge_game
+#endif  // GAME_H_
--- register.h
+++ register.h
@@ -1,33 +1,63 @@
 // File: register.h
 //
 // Brief: Macro-free registration utilities. The interface that enables a base
-//        class to create child classes by name, with no generator arguments.
-//        C++11 is required.
+//        class to create child classes by name, with custom but uniform
+//        constructor arguments. C++11 is required.
 //
 // Author: Pufan He <hpfdf@126.com>
 //
-// Version: 2014/08/19
+// Version: 2014/08/21
 //
-// Usage: Write your own Base or Child classes that extend RegisterBase or
-//        Register to enable the create-by-name feature.
+// Usage: Write your own Base class extending
+//            RegisterBase<Base, constructor_arguments_types...>.
+//        Write your own Child classes extending
+//            Register<Child, Base, registered_name>.
+//        Then you can create new Child by a string of registered name.
 //
-//        class Base: RegisterBase<Base>
+//        class Base: RegisterBase<Base, Argument...>
 //
-//          static Base* Create(const string& name);
-//          >> Create a new registered child class of Base. If name is empty,
-//             new Base is returned. Caller takes ownership of returned
-//             pointer. If no class is created, return nullptr.
+//          static Base* Create(const string& name, Argument... args);
+//          >> Create a new registered child class of Base. i.e.,
+//                 new Child(args...)
+//             will be returned, where Child is registered by the name.
+//          >> If no class is created in case of error, return nullptr.
+//          >> Caller takes ownership of returned pointer.
+//          >> args can be empty or any function argument configures, e.g.
+//                 class Base: RegisterBase<Base, int, string> {
+//                  public:
+//                   Base(int id, string msg) {...}
+//                   ...
+//                 }
+//          >> It also has the following creator variations. The memory of
+//             created objects Will be automatically cleared if using these:
+//                 static Base::uptr CreateUnique(name, ...);
+//                 static Base::ptr CreateShared(name, ...);
+//             where
+//                 Base::uptr = std::unique_ptr<Base>;
+//                 Base::ptr = std::shared_ptr<Base>;
 //
 //          static bool HasChild(const string& name);
 //          >> Return whether a name is registered by any child class of Base.
 //
 //          static bool SetChild<Child>(const string& name);
 //          >> Manually register a child class by name. Will fail if try to
-//             register a duplicated name. Return successful or not.
+//             register a duplicated name, or Child is not Registered.
+//          >> Return successful or not.
 //
 //          static bool RemoveChild(const string& name);
 //          >> Remove a registered name, so Base cannot create the associated
-//             child with name. Return true if a child is removed.
+//             child with name after this is called.
+//          >> Return true if a child is removed.
+//
+//          static vector<string> GetChildren();
+//          >> Return a list of names of all current registered children, in
+//             alphabetic order.
+//
+//          virtual const char* name();
+//          >> Get the runtime name of this object which is or extends Base.
+//
+//          virtual const char* info();
+//          >> Get the introduction of this registered object.
 //
 //        class Child: Register<Child, Base, Name>
 //
@@ -39,6 +69,10 @@
 //                 class Child: Register<Child, Base>
 //             The Base class does not register the Child class for create.
 //             However, user may use SetName() later to enable the register.
+//
+//             Child must have constructor functions with all configures of
+//             arguments which aer declared in all RegisterBases extended by
+//             Base.
 //
 //          static const char* GetName();
 //          >> Get the registered name of this child class.
@@ -71,45 +105,59 @@
 //         Fruit::Create("FruitInBox3");
 //
 
-#include <unordered_map>
+#ifndef REGISTER_H_
+#define REGISTER_H_
+
+#include <vector>
 #include <string>
+#include <memory>
+#include <unordered_map>
 
-namespace {
-
-template <class Base>
+template <class Base, typename... Argument>
 struct RegisterCreatorBase {
-  virtual Base* Create() {
+  virtual Base* Create(Argument...) {
     return nullptr;
   }
 };
 
-template <class Child, class Base>
-struct RegisterCreator: RegisterCreatorBase<Base> {
-  Base* Create() override {
-    return new Child;
+template <class Child, class Base, typename... Argument>
+struct RegisterCreator: RegisterCreatorBase<Base, Argument...> {
+  Base* Create(Argument... args) override {
+    return new Child(args...);
   }
 };
 
-template <class Child, class Base>
+template <class Child, class Base, typename... Argument>
 class RegisterCreatorContainer{
  public:
-  static RegisterCreatorBase<Base>* GetCreator() {
+  static RegisterCreatorBase<Base, Argument...>* GetCreator() {
     return &creator_;
   }
  private:
-  static RegisterCreator<Child, Base> creator_;
+  static RegisterCreator<Child, Base, Argument...> creator_;
 };
 
-template <class Base>
+template <class Base, typename... Argument>
 class RegisterBase {
  public:
-  static Base* Create(const std::string& name = "") {
-    if (name.empty())
-      return new Base;
+  typedef std::unique_ptr<Base> uptr;
+  typedef std::shared_ptr<Base> ptr;
+  typedef std::unordered_map<std::string, RegisterCreatorBase<Base, Argument...>*>
+      RegisterTable;
+
+  static Base* Create(const std::string& name, Argument... args) {
     if (!creators_.count(name))
       return nullptr;
     else
-      return creators_[name]->Create();
+      return creators_[name]->Create(args...);
+  }
+
+  static uptr&& CreateUnique(const std::string& name, Argument... args) {
+    return uptr(Create(name, args...));
+  }
+
+  static ptr&& CreateShared(const std::string& name, Argument... args) {
+    return ptr(Create(name, args...));
   }
 
   static bool HasChild(const std::string& name) {
@@ -135,8 +183,21 @@ class RegisterBase {
     return false;
   }
 
-  typedef std::unordered_map<std::string, RegisterCreatorBase<Base>*>
-      RegisterTable;
+  static std::vector<std::string> GetChildren() {
+    std::vector<std::string> result;
+    for (const auto& it: creators_)
+      result.push_back(it->first);
+    return result;
+  }
+
+  virtual const char* name() {
+    return "";
+  }
+
+  virtual const char* info() {
+    return "Register base class.";
+  }
+
  private:
   static RegisterTable creators_;
 };
@@ -149,7 +210,7 @@ class RegisterActivator {
     if (!registered) {
       registered = true;
       if (Name != nullptr)
-        RegisterBase<Base>::template SetChild<Child>(Name);
+        Base::SetChild<Child>(Name);
     }
   }
 };
@@ -177,6 +238,14 @@ class Register: public Base {
     return false;
   }
 
+  virtual const char* info() override {
+    return ("Registered sub-class \"" + name_ + "\".").c_str();
+  };
+
+  const char* name() override {
+    return name_.c_str();
+  }
+
  private:
   void activate() {
     if (Name != nullptr)
@@ -187,13 +256,16 @@ class Register: public Base {
   static std::string name_;
 };
 
-template <class Base>
-typename RegisterBase<Base>::RegisterTable RegisterBase<Base>::creators_;
+template <class Base, typename... Argument>
+typename RegisterBase<Base, Argument...>::RegisterTable
+RegisterBase<Base, Argument...>::creators_;
 
-template <class Child, class Base>
-RegisterCreator<Child, Base> RegisterCreatorContainer<Child, Base>::creator_;
+template <class Child, class Base, typename... Argument>
+RegisterCreator<Child, Base, Argument...>
+RegisterCreatorContainer<Child, Base, Argument...>::creator_;
 
 template <class Child, class Base, const char* Name>
-RegisterActivator<Child, Base, Name> Register<Child, Base, Name>::activator_;
+RegisterActivator<Child, Base, Name>
+Register<Child, Base, Name>::activator_;
 
-}  // namespace
+#endif  // REGISTER_H_
